48d0b11e9cd27f3a010950e810b23228c248fec7
Sanghyun Yi <njsharry32@gmail.com>
2024-04-11 14:37:49 +0900

    advanced now medicine init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Advanced Now Medicine `Android` (Refactoring & Migration)
---
+++ app/src/main/java/android/advanced/nowmedicine/MainActivity.kt
@@ -0,0 +1,46 @@
+package android.advanced.nowmedicine
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import android.advanced.nowmedicine.ui.theme.AdvancedNowMedicineTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AdvancedNowMedicineTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AdvancedNowMedicineTheme {
+        Greeting("Android")
+    }
+}
